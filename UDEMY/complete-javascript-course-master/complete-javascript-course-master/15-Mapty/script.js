12ef9d6731f2e5b0f5cbcf4b1bd32fb5c39b1946
신유수 <sin-yusu@sin-yusuui-MacBookPro.local>
2023-05-16 17:21:26 +0900

    mapty alone done

--- UDEMY/complete-javascript-course-master/complete-javascript-course-master/15-Mapty/script.js
+++ UDEMY/complete-javascript-course-master/complete-javascript-course-master/15-Mapty/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = ['January', 'February', 'March', 'April', 'May', 
+'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -10,16 +11,21 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+const workoutList = document.querySelector('.workout');
 
-let markerOn = true;
+let bool = true;
+let map
 
 const workOutsArr = [];
-let index = 0;
+const AllWorkOuts = localStorage.getItem('workouts');
+let index = !JSON.parse(AllWorkOuts) ? 0 :
+ JSON.parse(AllWorkOuts).length;
 
 class coordsCl{
-    constructor(lat,lng){
+    constructor(lat,lng,marker){
         this.lat = lat
         this.lng = lng
+        this.marker = marker
     }
     get coord(){
         return [this.lat,this.lng]
@@ -30,83 +36,109 @@ class coordsCl{
 }
 
 class workouts extends coordsCl{
-    constructor(lat,lng,type,dis,dur,cad,elev){
-        super(lat,lng)
+    date = new Date();
+    constructor(lat,lng,marker,index,type,dis,dur,other){
+        super(lat,lng,marker)
+        this.index = index
         this.type = type 
         this.dis = dis 
         this.dur = dur 
-        this.type === 'running' ? this.other = cad : this.other = elev
+        this.other = other
     }
 
     get popUpMessage (){
 
         const type = this.type.slice(0,1).toUpperCase() + this.type.slice(1)
-        const month = months[new Date().getMonth()];
-        const day = new Date().getDate();
-        return `${type} on ${month} ${day}`
+        const month = months[this.date.getMonth()];
+        const day = this.date.getDate();
+        return `${this.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${type} on ${month} ${day}`
     }
 
-    get popUp(){
-        const pop = L.popup()
-        .setLatLng(this.coord)
-        .setContent(this.popUpMessage);
-
-        return pop
+    get popUpClass(){
+        return this.type === 'running' ? 'running-popup' : 'cycling-popup'
     }
 
+    stampMarker(){
+        this.clickMarker().addTo(map)
+        .bindPopup(L.popup({
+            autoClose:false,
+            maxWidth:250,
+            minWidth:100,
+            closeOnClick:false,
+            className:this.popUpClass
+        }))
+        .setPopupContent(this.popUpMessage)
+        .openPopup();
+    }
 }
-
-
 
 navigator.geolocation.getCurrentPosition(function(pos){
     const {latitude,longitude} = pos.coords;
 
     const coords = [latitude,longitude];
 
-    const map = L.map('map').setView(coords, 15);
+    map = L.map('map').setView(coords, 15);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">'
+        +'OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    let workOutArrs = JSON.parse(AllWorkOuts);
+
+    if( workOutArrs !== null){
+        workOutArrs.forEach(e => {
+            const eWorkouts = 
+            new workouts(e.lat,e.lng,e.marker,e.index,e.type,e.dis,e.dur,e.other);
+            eWorkouts.stampMarker();
+            containerWorkouts.insertAdjacentHTML('beforeend',
+            makeWorkoutList(e.index,eWorkouts));
+        })
+    }
 
     map.on('click',function(mapEvent){
 
         form.classList.remove('hidden');
+        inputDistance.focus();
+        inputType.value = 'running'
         
         const {lat,lng} = mapEvent.latlng;
-        const coord = new coordsCl(lat,lng)
-        
-        const clickMarker = L.marker(coord.coord);
-        console.log(coord.clickMarker())
-        if(markerOn === true){
+        const coord = new coordsCl(lat,lng,true)
+
+        if(coord.marker === true){
           coord.clickMarker().addTo(map);
-          markerOn = false;
+          coord.marker = false;
         } else return
 
         document.addEventListener('keydown',function(e){
 
-            if(e.code === 'Enter' && markerOn === false){
-
-                const workout = new workouts(lat,lng,inputType.value,inputDistance.value,inputDuration.value,inputCadence.value,inputElevation.value)
+            if(e.code === 'Enter' && coord.marker === false){
+                const workoutIndex = index++
+                const workout = new workouts(lat,lng,true,workoutIndex,
+                    inputType.value,
+                    inputDistance.value,
+                    inputDuration.value,
+                    bool === !bool ? inputElevation.value : inputCadence.value
+                )
 
                 workOutsArr.push(workout);
 
-                localStorage.setItem(`workouts`, JSON.stringify(workOutsArr));
+                let alreadyWorkouts = JSON.parse(localStorage.getItem('workouts'))
+                if(alreadyWorkouts === null) {
+                    localStorage.setItem(`workouts`, JSON.stringify(workOutsArr));
+                } else {
+                    localStorage.setItem(`workouts`,
+                    JSON.stringify(alreadyWorkouts.concat(workOutsArr)));
+                }
 
-                console.log(workout)
+                workout.stampMarker()
 
-                workout.clickMarker()
-                .bindPopup(`${workout.popUpMessage}`)
-                .openPopup();
-
-                // workout.clickMarker()
-                workout.popUp.openOn(map);
-
-                markerOn = true;
+                coord.marker = true;
 
                 form.classList.add('hidden');
                 
-                containerWorkouts.insertAdjacentHTML('beforeend',workoutLi(index++,workout));
+                containerWorkouts.insertAdjacentHTML('beforeend',
+                makeWorkoutList(workoutIndex,workout));
                 nullMaker([inputDistance,inputDuration,inputCadence,inputElevation]);
                 e.preventDefault();
             }
@@ -127,7 +159,33 @@ const objMaker = (a,b,c,d,e)=> {
     return obj
 }
 
-const workoutLi = (index,obj) => {
+containerWorkouts.addEventListener('click',function(e){
+    const check =  e.target.classList.value.indexOf('workout')
+    if(check === 0){
+        const getId = e.target.closest('.workout').dataset.id;
+        const toFindArr = JSON.parse(AllWorkOuts);
+
+        const result = toFindArr.find(e=> e.index === +getId);
+        const {lat,lng} = result;
+
+        map.setView([lat,lng],15)
+    }
+})
+
+inputType.addEventListener('change',function(e){
+    bool = !bool
+    if(bool){
+        inputCadence.className = inputCadence.className;
+        inputCadence.previousElementSibling.innerHTML = 'Cadence';
+        inputCadence.placeholder = 'step/min'
+    }else if(!bool){
+        inputCadence.className = inputElevation.className
+        inputCadence.previousElementSibling.innerHTML = 'Elevation';
+        inputCadence.placeholder = 'meter'
+    }
+})
+
+const makeWorkoutList = (index,obj) => {
 return    `<li class="workout workout--${obj.type}" data-id="${index}">
 <h2 class="workout__title">${obj.popUpMessage}</h2>
 <div class="workout__details">
